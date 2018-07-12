ca6fb55f7d06a39b7285b8842b7704c872ef04fc
Chinaza-Deborah <chinazadeborah073@gaim.com>
2018-07-12 10:50:08 +0100

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -10,8 +10,19 @@ var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
+ 
 io.on("connection", (socket) => {
     console.log("New user connected");
+
+   socket.emit("newMessage", {
+       from: "Deborah",
+       text: "See you then",
+       createAt: 123123
+   });
+
+    socket.on("createMessage", (message) => {
+        console.log("createMessage", message);
+    });
 
     socket.on("disconnect", () => {
         console.log("User was disconnected");
